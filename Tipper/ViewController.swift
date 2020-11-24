24c015d744052ab1a10236c2f4ae2a529f5e36f9
Jason Zheng <jaszheng2001@gmail.com>
2020-11-24 17:38:05 -0500

    Add required functionality

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    @IBOutlet weak var billAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
+    @IBOutlet weak var totalAmtLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentage = [0.1,0.15,0.2]
+        
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalAmtLabel.text = String(format: "$%.2f", total)
+    }
 }
 
